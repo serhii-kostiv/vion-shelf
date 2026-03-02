ea8aaa6ab5e1827f3af6e5e891884ffb4dfd953b
serhii-kostiv <kostiv.serhii@gmail.com>
2026-03-02 23:36:37 +0200

    VION-10: add axios

--- frontend/app/pages/profile.vue
+++ frontend/app/pages/profile.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="flex flex-col items-center justify-center">
     <h1 class="text-3xl font-semibold">Profile</h1>
-    <p class="mt-4">Welcome, {{ data?.name || data?.email }}!</p>
-    <button @click="test">Test</button>
+    <p class="mt-4">Welcome, {{ data?.name || data?.email || "empty" }}!</p>
+    <p>Status:::{{ status }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
 const { status, data } = useAuth();
-
-const test = () => {
-  const { data: response } = useApi("test");
-  console.log(response);
-};
 </script>
